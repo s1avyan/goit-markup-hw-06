552f12f99986d318444042f5ed083c8eedf9f036
Andrey <s1avyan27@gmail.com>
2020-08-02 00:17:39 +0300

    start HW-6

---
+++ js/s1avyan.js
@@ -0,0 +1,27 @@
+// плавная прокрутка
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+// плавная прокрутка
+
+// Google map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+//  /google map
+
+// API - application programming interface
+console.log("Privet");
