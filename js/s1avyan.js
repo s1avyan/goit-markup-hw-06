06a0efde31625dceb4295f3452185ae50ff08fb4
Andrey <s1avyan27@gmail.com>
2020-12-11 16:53:27 +0200

    мелкие правки

--- js/s1avyan.js
+++ js/s1avyan.js
@@ -25,3 +25,19 @@ function initMap() {
 
 // API - application programming interface
 console.log("Privet");
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myBtn');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal)
+  modal.style.display = "none";
+}
